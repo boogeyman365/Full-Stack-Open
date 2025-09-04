5b14878fd194d7536a2067ac6427f3be2a14a9ae
Boogey <oscarestablie@hotmail.com>
2025-09-04 10:45:12 +0200

    new

--- rate-repository-app/eslint.config.js
+++ rate-repository-app/eslint.config.js
@@ -7,4 +7,21 @@ module.exports = defineConfig([
   {
     ignores: ['dist/*'],
   },
+  {
+    plugins: ["react"],
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parser: "babel-eslint",
+    env: {
+      browser: true
+    },
+    rules: {
+      "react/prop-types": "off",
+      "semi": "error"
+    }
+  }
 ]);
